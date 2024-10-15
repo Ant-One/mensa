ae76f9a385e36235181b1adc5c8566a062c5cedb
Antoine Moix <antoine.moix@gmail.com>
2024-10-15 11:47:11 +0200

    style: ran ruff on files

--- mensa/app/models.py
+++ mensa/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Mensa(models.Model):
     """
     Model for a mensa (cafeteria)
--- mensa/app/views.py
+++ mensa/app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def testView(request):
-    return(HttpResponse("Hello"))
+    return HttpResponse("Hello")
